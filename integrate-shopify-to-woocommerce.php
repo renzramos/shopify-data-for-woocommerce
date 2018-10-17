abd26560ba3c737277af3206cf45b0c8f24278f1
Renz Ramos <im.renzramos@gmail.com>
2018-10-17 21:57:20 +0800

    Updated file

--- integrate-shopify-to-woocommerce.php
+++ integrate-shopify-to-woocommerce.php
@@ -11,16 +11,10 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  istw
 */
 
-class ShopifyWoocommerceIntegration
-{
-    /**
-     * Holds the values to be used in the fields callbacks
-     */
+class ShopifyWoocommerceIntegration{
+
     private $options;
 
-    /**
-     * Start up
-     */
     public function __construct()
     {
         add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
@@ -31,25 +25,17 @@ class ShopifyWoocommerceIntegration
 
 
         add_action( 'init', array( $this, 'update_images_from_shopify' ) );
-
-
     }
 
-    
-    public function init_metabox() {
-        add_action( 'add_meta_boxes', array( $this, 'add_metabox'  )        );
-        add_action( 'save_post', array( $this, 'save_metabox' ), 10, 2 );
-    }
-
-    public function add_plugin_page()
-    {
+    /*
+    * Dashboard 
+    */
+    public function add_plugin_page(){
         add_menu_page('SH + WC', 'Shopify + WC', 'manage_options', 'sh-wc-setting-page', array( $this, 'admin_page' ), 'dashicons-admin-links');
     }
 
-  
-    public function admin_page()
-    {
-        // Set class property
+    public function admin_page(){
+
         $this->options = get_option( 'sh_wc_options' );
         $shopify_api_key = (isset($this->options['shopify_api_key'])) ? $this->options['shopify_api_key'] : '';
 
@@ -94,8 +80,7 @@ class ShopifyWoocommerceIntegration
         <?php
     }
 
-    public function page_init()
-    {        
+    public function page_init(){        
         register_setting(
             'my_option_group', // Option group
             'sh_wc_options'
@@ -131,14 +116,13 @@ class ShopifyWoocommerceIntegration
         <?php
     }
 
-
-    public function generate_shopify_endpoint($api_key){
-        return 'https://'  . $api_key . '@shiny-leaf.myshopify.com/';
+    /*
+    * Metabox 
+    */
+    public function init_metabox() {
+        add_action( 'add_meta_boxes', array( $this, 'add_metabox'  )        );
+        add_action( 'save_post', array( $this, 'save_metabox' ), 10, 2 );
     }
-
-
-
-
     public function add_metabox() {
         add_meta_box( 'istw-meta-box',  __( 'Shopify Data', 'istw' ), array( $this, 'render_metabox' ), 'product', 'advanced', 'default');
     }
@@ -170,8 +154,47 @@ class ShopifyWoocommerceIntegration
         <?php 
         }
     }
-    
 
+    public function save_metabox( $post_id, $post ) {
+        // Add nonce for security and authentication.
+        $nonce_name   = isset( $_POST['custom_nonce'] ) ? $_POST['custom_nonce'] : '';
+        $nonce_action = 'custom_nonce_action';
+ 
+        // Check if nonce is set.
+        if ( ! isset( $nonce_name ) ) {
+            return;
+        }
+ 
+        // Check if nonce is valid.
+        if ( ! wp_verify_nonce( $nonce_name, $nonce_action ) ) {
+            return;
+        }
+ 
+        // Check if user has permissions to save data.
+        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return;
+        }
+ 
+        // Check if not an autosave.
+        if ( wp_is_post_autosave( $post_id ) ) {
+            return;
+        }
+ 
+        // Check if not a revision.
+        if ( wp_is_post_revision( $post_id ) ) {
+            return;
+        }
+
+        if ($_REQUEST['shopify-product-id']){
+            update_post_meta($post_id,'shopify_product_id',$_REQUEST['shopify-product-id']);
+        }
+    }
+
+
+    /* 
+    * Updater
+    */
+    
     public function update_images_from_shopify(){
         if ($_GET['update-shopify-products-images']){
 
@@ -222,6 +245,9 @@ class ShopifyWoocommerceIntegration
         }
     }
 
+    /*
+    * Helper
+    */
     public function get_shopify_product_images($shopify_product_id){
 
         $this->options = get_option( 'sh_wc_options' );
@@ -234,40 +260,11 @@ class ShopifyWoocommerceIntegration
         return $data->products[0]->images;
     }
     
-    public function save_metabox( $post_id, $post ) {
-        // Add nonce for security and authentication.
-        $nonce_name   = isset( $_POST['custom_nonce'] ) ? $_POST['custom_nonce'] : '';
-        $nonce_action = 'custom_nonce_action';
- 
-        // Check if nonce is set.
-        if ( ! isset( $nonce_name ) ) {
-            return;
-        }
- 
-        // Check if nonce is valid.
-        if ( ! wp_verify_nonce( $nonce_name, $nonce_action ) ) {
-            return;
-        }
- 
-        // Check if user has permissions to save data.
-        if ( ! current_user_can( 'edit_post', $post_id ) ) {
-            return;
-        }
- 
-        // Check if not an autosave.
-        if ( wp_is_post_autosave( $post_id ) ) {
-            return;
-        }
- 
-        // Check if not a revision.
-        if ( wp_is_post_revision( $post_id ) ) {
-            return;
-        }
-
-        if ($_REQUEST['shopify-product-id']){
-            update_post_meta($post_id,'shopify_product_id',$_REQUEST['shopify-product-id']);
-        }
+    public function generate_shopify_endpoint($api_key){
+        return 'https://'  . $api_key . '@shiny-leaf.myshopify.com/';
     }
+
+    
 
 }
 $shopify_woocommerce_integration = new ShopifyWoocommerceIntegration();
